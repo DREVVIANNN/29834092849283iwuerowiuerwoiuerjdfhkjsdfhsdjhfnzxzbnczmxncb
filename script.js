1d60bb1892aafbdee1b0056cc1c8668092706d0b
DREVVIANN <nekotopias3@gmail.com>
2025-03-14 12:23:35 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -86,31 +86,6 @@ const firebaseConfig = {
   };
 
   firebase.initializeApp(firebaseConfig);
-
-  document.getElementById('login-google').addEventListener('click', function(){
-    var provider = new firebase.auth.GoogleAuthProvider();
-
-    firebase.auth()
-  .signInWithPopup(provider)
-  .then((result) => {
-    /** @type {firebase.auth.OAuthCredential} */
-    var credential = result.credential;
-
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // IdP data available in result.additionalUserInfo.profile.
-      // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-    alert(errorMessage);
-  });
-  })
+        const auth = firebase.auth();
+        const db = firebase.firestore();
+        const storage = firebase.storage();
